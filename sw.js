50e3e61462d0b165389a46f00e62415ace4c8e7b
laliyael <122647424+laliyael@users.noreply.github.com>
2024-12-11 19:56:37 -0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,38 @@
-//Asignar nombre y versión de la cache
-const CACHE_NAME = 'v1_cache_LaliPWA';
+//Asignar nombre y version de la cache
+const CACHE_NAME = 'v1_cache_LaliYaelPWA';
 
-//ficheros a cachear en la aplicación
-var urlsToCache = [
- 	'./',
- 	'./styles.css',
- 	'./img/1.png',
- 	'./img/2.png',
- 	'./img/3.png',
- 	'./img/facebook.png',
- 	'./img/instagram.png',
- 	'./img/twitter.png',
- 	'./img/favicon-1024.png',
- 	'./img/favicon-512.png',
- 	'./img/favicon-384.png',
- 	'./img/favicon-256.png',
- 	'./img/favicon-128.png',
- 	'./img/favicon-96.png',
- 	'./img/favicon-64.png',
- 	'./img/favicon-32.png',
- 	'./img/favicon-16.png'
-	];
+//ficheros a cachear en la aplicacion
+var urlsToCache=[
+    './',
+    './styles.css',
+	'./img/1.png',
+    './img/2.png',
+    './img/3.png',
+    './img/instagram.png',
+    './img/facebook.png',
+	'./img/twitter.png',
+    './img/favicon-1024.png',
+    './img/favicon-512.png',
+    './img/favicon-384.png',
+    './img/favicon-256.png',
+    './img/favicon-192.png',
+    './img/favicon-128.png',
+    './img/favicon-96.png',
+    './img/favicon-64.png',
+    './img/favicon-32.png',
+    './img/favicon-16.png',
+];
 
 //Evento install
-// Instalación del service Worker y guarda en cache los recursos estaticos
-self.addEventListener('install', e => {
+// Instalacion del service worker y guarda en cache los recursos 
+self.addEventListener('install',e=> {
     e.waitUntil(
         caches.open(CACHE_NAME)
         .then(cache => {
             return cache.addAll(urlsToCache)
-            	.then(() => {
-            		self.skipWaiting();
-            	});
-            	
+            .then(()=> {
+                self.skipWaiting();
+            });
         })
         .catch(err => console.log('No se ha registrado el cache', err))
     );
